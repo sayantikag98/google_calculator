07072843124fb1debac360b0285c58c529a56f88
Sayantika Ghosh <sayantikaghosh98@gmail.com>
2021-12-04 18:54:12 +0530

    Added basic calculation functionality

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ const operator = document.querySelectorAll(".operator");
 const ansOnState = document.querySelectorAll(".on");
 const currentAns = document.getElementById("current-answer");
 const equal = document.getElementById("equal");
+const historyPara = document.getElementById("history-para");
+const clear = document.getElementById("clear");
+const inverse = document.getElementById("inverse");
+const sinBtn = document.querySelector(".sin");
+const lnBtn = document.querySelector(".ln");
+const cosBtn = document.querySelector(".cos");
+const logBtn = document.querySelector(".log");
+const tanBtn = document.querySelector(".tan");
+const sqrtBtn = document.querySelector(".sqrt");
+const ansBtn = document.querySelector(".ans");
+const powerBtn = document.querySelector(".power");
+const radian = document.querySelector("#radian");
+const degree = document.querySelector("#degree");
+const percentage = document.querySelector("#percent");
+
+
+inverse.setAttribute("style","backgroundColor:red")
 
 let decimalCount = 0;
 let numInput = "";
@@ -27,6 +44,9 @@ historyBtnInsideDiv.addEventListener("click", () => {
 
 ansOnState.forEach(ele => {
     ele.addEventListener("click", () => {
+        if(clear.textContent === "AC"){
+            clear.textContent = "CE";
+        }
         if(currentAns.textContent === ""){
             currentAns.textContent = "Ans = 0";
         }
@@ -49,7 +69,10 @@ number.forEach(ele => {
             numInput="";
             exp="";
         }
-
+        else if(numInput.charAt(numInput.length - 1) === "%"){
+            numInput+=String.fromCharCode(215);
+            exp+="*";
+        }
         if(ele.textContent === ".")
             decimalCount++;
 
@@ -57,6 +80,9 @@ number.forEach(ele => {
             numInput+=ele.textContent;
             exp+=ele.textContent;
             currentCalculations.textContent = numInput;
+        }
+        if(clear.textContent === "AC"){
+            clear.textContent = "CE";
         }
         
     });
@@ -70,7 +96,7 @@ operator.forEach(ele => {
             numInput+="0";
             exp+="0";
         }
-        if(numInput.length>0 && ((!Number.isNaN(Number(numInput.charAt(numInput.length - 1)))) || (numInput.charAt(numInput.length - 1) === "="))){
+        if(numInput.length>0 && ((!Number.isNaN(Number(numInput.charAt(numInput.length - 1)))) || (numInput.charAt(numInput.length - 1) === "=") || (numInput.charAt(numInput.length - 1) === "%"))){
             if(numInput.length > 0 && numInput.charAt(numInput.length - 1) === "="){
                 exp = ans;
                 numInput = ans;
@@ -92,6 +118,9 @@ operator.forEach(ele => {
             currentCalculations.textContent = numInput;
             decimalCount = 0;
         }
+        if(clear.textContent === "AC"){
+            clear.textContent = "CE";
+        }
     });
 });
 
@@ -100,12 +129,113 @@ operator.forEach(ele => {
 equal.addEventListener("click", () => {
     if(numInput.length > 0 && numInput.charAt(numInput.length - 1) !== "="){
         ans = math.evaluate(exp);
+        
+        historyPara.style.display = "none";
+        const calcHistMostOuterDiv = document.createElement("div");
+        const calcHistOuterDiv = document.createElement("div");
+        const calcHistExpDiv = document.createElement("div");
+        const calcHistEqualDiv = document.createElement("div");
+        const calcHistAnsDiv = document.createElement("div");
+        
+        if((historyDiv.lastElementChild.nodeName === "P")||(historyDiv.lastElementChild.nodeName === "DIV" && historyDiv.lastElementChild.firstElementChild.textContent !== numInput)){
+            calcHistEqualDiv.textContent = "=";
+            calcHistExpDiv.textContent = numInput;
+            calcHistAnsDiv.textContent = ans;
+            calcHistOuterDiv.appendChild(calcHistExpDiv);
+            calcHistOuterDiv.appendChild(calcHistEqualDiv);
+            calcHistOuterDiv.appendChild(calcHistAnsDiv);
+            calcHistMostOuterDiv.appendChild(calcHistOuterDiv);
+            historyDiv.appendChild(calcHistOuterDiv);
+    
+            calcHistOuterDiv.setAttribute("style", "display:flex;alignItems:center;position:relative;top:50px;marginRight:20px;")
+            calcHistExpDiv.setAttribute("style", "border:1px solid gray;padding:10px;borderRadius:10px;color:blue;margin:10px;");
+            calcHistAnsDiv.setAttribute("style", "border:1px solid gray;padding:10px;borderRadius:10px;color:blue;margin:10px;");
+            calcHistEqualDiv.setAttribute("style", "fontSize:40px;margin:10px 0;padding:10px;");
+            calcHistMostOuterDiv.setAttribute("style", "position:relative;overflowY:scroll;height:200px;top:(100px);")
+        }
+        
+
+
         numInput+=" =";
         currentAns.textContent = numInput;
         currentCalculations.textContent = ans;
         decimalCount = 0;
+
+        if(clear.textContent === "CE"){
+            clear.textContent = "AC";
+        }
+        console.log(clear);
     }
 
+});
+
+//clear
+
+clear.addEventListener("click", () => {
+    if(currentAns.textContent === ""){
+        currentAns.textContent = "Ans = 0";
+    }
+    if(clear.textContent === "CE"){
+        numInput = numInput.substring(0,numInput.length-1);
+        exp = exp.substring(0, exp.length - 1);
+        if(numInput.length>0)
+            currentCalculations.textContent = numInput; 
+        else
+            currentCalculations.textContent = "0";
+    }
+    else if (clear.textContent === "AC"){
+        currentAns.textContent = `Ans = ${ans}`;
+        currentCalculations.textContent = "0";
+        numInput = "";
+        exp = "";
+        clear.textContent = "CE";    
+    }
+});
+
+// inverse
+
+inverse.addEventListener("click", () => {
+    const invColor = inverse.getAttribute("style").substring(16);
+    console.log(invColor);
+    if(invColor === "red")
+        inverse.setAttribute("style","backgroundColor:red");
+    else
+        inverse.setAttribute("style","backgroundColor:blue");
+    sinBtn.innerHTML = sinBtn.innerHTML === "sin" ? "sin"+"-1".sup() : "sin";
+    lnBtn.innerHTML = lnBtn.innerHTML === "ln" ? "e"+"x".sup() : "ln";
+    cosBtn.innerHTML = cosBtn.innerHTML === "cos" ? "cos"+"-1".sup() : "cos";
+    logBtn.innerHTML = logBtn.innerHTML === "log" ? "10"+"x".sup() : "log";
+    tanBtn.innerHTML = tanBtn.innerHTML === "tan" ? "tan"+"-1".sup() : "tan";
+    sqrtBtn.innerHTML = sqrtBtn.innerHTML.charCodeAt(0) === 8730 ? "x"+"2".sup() : String.fromCharCode(8730);
+    ansBtn.innerHTML = ansBtn.innerHTML === "Ans" ? "Rnd" : "Ans";
+    powerBtn.innerHTML = powerBtn.innerHTML.charCodeAt(0) === 120 ? "n".sup()+String.fromCharCode(8730)+"x" : String.fromCharCode(120);
+});
+
+// radian
+
+degree.addEventListener("click", () => {
+    radian.disabled = false;
+    degree.disabled = true;
+});
+
+radian.addEventListener("click", () => {
+    radian.disabled = true;
+    degree.disabled = false;
+});
+
+percentage.addEventListener("click", () => {
+    if(numInput.length === 0){
+        numInput+="0";
+        exp+="0";
+    }
+    else if(numInput.length>0 && numInput.charAt(numInput.length - 1) === "="){
+        numInput = ans;
+        exp = ans;
+        currentAns.textContent = `Ans = ${ans}`;
+    }
+    numInput+=percentage.textContent;
+    exp+=percentage.textContent;
+    currentCalculations.textContent = numInput;
 });
 
 
